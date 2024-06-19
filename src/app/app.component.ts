61748df612983ca2e4dda51d86f63bc377179a9e
iSaan86 <sandraferreraarenas@gmail.com>
2024-06-19 21:10:13 +0200

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgModel } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MiniCalculadoraApp';
+  title = 'Mini Calculadora';
+  numero1:number=0;
+  numero2:number=0;
+  resultado:number=0;
+
+  sumar():void{
+
+    this.resultado=this.numero1+this.numero2;
+
+  }
+
+  restar():void{
+
+    this.resultado=this.numero1-this.numero2;
+
+  }
+
+  multiplicar():void{
+
+    this.resultado=this.numero1*this.numero2;
+
+  }
+
+  dividir():void{
+
+    this.resultado=this.numero1/this.numero2;
+  }
+
 }
